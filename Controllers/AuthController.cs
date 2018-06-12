924790096fee73186e6e5a7ee5df166534970bbc
infazse <infaz.10@cse.mrt.ac.lk>
2018-06-12 23:08:14 +0530

    Removed the exception

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -53,7 +53,7 @@ namespace DatingApp.API.Controllers
         public async Task<IActionResult> Login([FromBody]UserForLoginDto userForLoginDto)
         {
 
-            throw new Exception("Computer says no");   
+            //throw new Exception("Computer says no");   
             var userFromRepo = await _repo.Login(userForLoginDto.userName.ToLower(), userForLoginDto.password);
 
             if (userFromRepo == null)
